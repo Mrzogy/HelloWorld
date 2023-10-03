a59398398e4205f38f00d8d95e9b381eb2c19b10
Abdulrazaq Ali <121042333+Mrzogy@users.noreply.github.com>
2023-10-03 22:42:44 +0300

    File

---
+++ Program.cs
@@ -0,0 +1,9 @@
+﻿// See https://aka.ms/new-console-template for more information
+Console.WriteLine("Hello, World!");
+Console.WriteLine("Abdulrazaq Ali");
+
+Console.WriteLine("Please Enter Your Name: ");
+
+String name = Console.ReadLine();
+
+Console.WriteLine("Hello " + name);
